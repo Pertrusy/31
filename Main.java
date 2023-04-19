4f4523910d765802498d68d07da2a1eca54561b2
Pertrusy <127326311+Pertrusy@users.noreply.github.com>
2023-04-19 14:11:01 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    int high = 5;
+    for(int i = 0; i<high; i++){
+      for(int k = 0; k<= i; k++){
+        System.out.print("*");
+      }
+      for(int j = 1; j < 5-i ;j++){
+        System.out.print("0");
+      }
+      System.out.println();
+    }
+  }
+}
